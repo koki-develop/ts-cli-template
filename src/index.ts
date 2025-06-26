dc4f2a09a36abb51a0205306dd461e22e4ea4db2
koki-develop <kou.pg.0131@gmail.com>
2025-06-26 19:29:19 +0900

    feat: Add command

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello via Bun!");
+import { Command } from "commander";
+import packageJson from "../package.json" with { type: "json" };
+
+const program = new Command();
+
+program
+  .name("TODO")
+  .version(packageJson.version)
+  .action(() => {
+    console.log("hello world");
+  });
+
+program.parse(process.argv);
